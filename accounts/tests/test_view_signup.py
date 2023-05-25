70fb93c15d69b866ce6aaf846a5fd3edad9edbdf
Coldplx <jjaramillod@uni.pe>
2023-05-25 17:26:48 -0500

    Starting: Creating Custom Template Tags | Tutorial Part 4

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -1,15 +1,13 @@
 from django.urls import reverse, resolve
 from django.test import TestCase
-from .views import signup
-from django.contrib.auth.forms import UserCreationForm
-from .views import signup
+from ..views import signup
 from django.contrib.auth.models import User
-from .forms import SignUpForm
+from ..forms import SignUpForm
 # Create your tests here.
 class SignUpTests(TestCase):
     def setUp(self):
         url = reverse('signup')
-        self.response = self.client(url)
+        self.response = self.client.get(url)
 
     def test_signup_status_code(self):
         self.assertEquals(self.response.status_code, 200)
@@ -41,6 +39,7 @@ class SuccessfulSignUpTests(TestCase):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'jjaramillod@uni.pe',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456'
         }
@@ -67,21 +66,20 @@ class SuccessfulSignUpTests(TestCase):
         self.assertTrue(user.is_authenticated)
 
 
+class InvalidSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {})  # submit an empty dictionary
 
-    class InvalidSignUpTests(TestCase):
-        def setUp(self):
-            url = reverse('signup')
-            self.response = self.client.post(url, {})  # submit an empty dictionary
+    def test_signup_status_code(self):
+        '''
+        An invalid form submission should return to the same page
+        '''
+        self.assertEquals(self.response.status_code, 200)
 
-        def test_signup_status_code(self):
-            '''
-            An invalid form submission should return to the same page
-            '''
-            self.assertEquals(self.response.status_code, 200)
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
 
-        def test_form_errors(self):
-            form = self.response.context.get('form')
-            self.assertTrue(form.errors)
-
-        def test_dont_create_user(self):
-            self.assertFalse(User.objects.exists())
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from boards import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -25,5 +26,7 @@ urlpatterns = [
     path("boards/<int:pk>/new/", views.new_topic, name="new_topic"),
     path('admin/', admin.site.urls),
     path('signup/', accounts_views.signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     
 ]
